44fddf4a2885a4d9a0afbf03e2eaa93a71f5e3b8
Juho Hänninen <juho.hanninen@cgi.com>
2022-12-27 20:39:20 +0200

    Include query params on redirect

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const url = require('url');
 
 const pysakitRouter = require('./stops-qrcode-redirect');
 
-
 const app = express();
-
 
 app.get('/', (req, res) => {
   res.redirect(301, 'https://reittiopas.fi/');
@@ -13,8 +12,10 @@ app.get('/', (req, res) => {
 app.use('/pysakit', pysakitRouter);
 
 // Here's the list of all frontend services where we want to add trailing slash
+// Includes also query params
 app.get(/^\/(kuljettaja|julkaisin|kartta|linjakartta|jore-import)$/, (req, res) => {
-  res.redirect(`${req.path}/`);
+  const { query } = url.parse(req.url);
+  res.redirect(`${req.path}/${query ? `?${query}` : ''}`);
 });
 
 app.get('/health', (req, res) => {
